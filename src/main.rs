533f28bbabbab9474b9783959978478aa5a5db76
Ben Kimock <kimockb@gmail.com>
2018-02-13 01:10:45 -0500

    Add config file parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+extern crate discord;
 #[macro_use]
 extern crate failure;
+extern crate futures;
+#[macro_use]
+extern crate serde_derive;
 extern crate serde_json;
 extern crate slack_api;
 extern crate termion;
+extern crate tokio_core;
+extern crate toml;
 extern crate websocket;
 
 use termion::raw::IntoRawMode;
@@ -14,19 +20,20 @@ use conn::Conn;
 mod slack_conn;
 use slack_conn::SlackConn;
 mod bimap;
+mod discord_conn;
+use discord_conn::DiscordConn;
 
-fn slack_api_key() -> String {
-    use std::fs::File;
-    use std::io::Read;
 
-    let mut file = File::open("/home/ben/slack_api_key").expect("Couldn't find API key");
-    let mut api_key = String::with_capacity(128);
-    file.read_to_string(&mut api_key)
-        .expect("Unable to read API key");
-    api_key.trim().to_owned()
+#[derive(Debug, Deserialize)]
+struct Config {
+    servers: Vec<conn::ServerConfig>,
 }
 
 fn main() {
+    use std::path::PathBuf;
+    use std::fs::File;
+    use std::io::Read;
+
     let _guard = std::io::stdout()
         .into_raw_mode()
         .expect("Couldn't put stdout into raw mode");
@@ -34,13 +41,24 @@ fn main() {
     let mut tui = TUI::new();
     tui.draw();
 
-    let slack_config = conn::ServerConfig::Slack {
-        token: slack_api_key(),
-    };
-    let connection =
-        SlackConn::new(slack_config, tui.sender()).expect("Failed to crate slack connection");
-    tui.add_server(connection);
+    let homedir = std::env::var("HOME").expect("You don't even have a $HOME? :'(");
+    let config_path = PathBuf::from(homedir).join(".omnichat.toml");
+    let mut contents = String::new();
+    File::open(config_path)
+        .expect("No config file found")
+        .read_to_string(&mut contents)
+        .expect("Couldn't read config file");
 
+    let config: Config = toml::from_str(&contents).expect("Config is not valid TOML");
+
+    for c in config.servers.into_iter() {
+        let connection = match c {
+            conn::ServerConfig::Slack{..} => SlackConn::new(c, tui.sender()),
+            conn::ServerConfig::Discord{..} => DiscordConn::new(c, tui.sender()),
+            _ => panic!("Unsupported config"),
+        };
+        tui.add_server(connection.unwrap());
+    }
     tui.draw();
     tui.run();
 }
